86e54ddbc70625248350227ee1fdecd3af5d19a6
Mattie <matthew.hall@serenetangerine.com>
2021-06-05 16:58:08 -0600

    bug fixes to launchMenu/menu.py (still not 100% working)

--- launchMenu/menu.py
+++ launchMenu/menu.py
@@ -1,6 +1,6 @@
 from machine import ADC, I2C, Pin
 from ssd1306 import SSD1306_I2C
-from temp import temperatureLoop
+#from temp import temperatureLoop
 from time import sleep
 
 
@@ -22,36 +22,39 @@ height = 64
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=100000)
 oled = SSD1306_I2C(width, height, i2c)
 
-scripts = ['temp.py', 'osciliscope.py', 'heart.py']
+scripts = ['temp.py', 'osc.py', 'heart.py']
 
 
 def drawScreen(selection):
     oled.fill(0)
     oled.text('Select Program:', 0, 0)
     for i in range(len(scripts)):
-        oled.text(scripts[i], 0, (15 * (i + 1)))
+        oled.text(scripts[i], 10, (15 * (i + 1)))
     oled.text('*', 0, (15 * selection))
     oled.show()
 
 
 selection = 1
+drawScreen(selection)
 while True:
     if upButton.value():
+        print('up button pressed!')
         if selection > 1:
-            selection -= selection
+            selection -= 1
             drawScreen(selection)
+        sleep(0.5)
 
     if downButton.value():
-        if selection <= len(scripts):
-            selection += selection
+        print('down button pressed!')
+        if selection < len(scripts):
+            selection += 1
             drawScreen(selection)
+        sleep(0.5)
 
     if enterButton.value():
+        print('enter button pressed!')
         oled.fill(0)
-        oled.text('Running %s...' % scripts[selection - 1])
+        oled.text('Running %s...' % scripts[selection])
         oled.show()
         sleep(2)
         # run script
-
-    sleep(0.5)
-        
