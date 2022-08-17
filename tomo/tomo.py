a78e6acfe9e5b90bf90d6d7934e03981bf1e6bc7
Mattie <matthew.hall@serenetangerine.com>
2022-08-16 21:53:00 -0600

    pico tomo respawn hearts refill

--- tomo/tomo.py
+++ tomo/tomo.py
@@ -42,12 +42,13 @@ class Tomo:
         self.x = 0 
         self.y = 32
 
+        self.health = -1
         self.spawn()
 
     def spawn(self):
-        self.health = 8
         self.dir = 1
         for i in range(9):
+            self.health = self.health + 1
             if i % 2 == 0:
                 self.sprite = self.egg1
             else:
